e18eacd7db589e1b0e21c3751b6e8a7778fa2b94
Asad Ullah <shaikhasad183@gmail.com>
2025-02-02 16:17:15 +0500

    fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import Home from "./pages/home/Home";
 import SignIn from "./pages/Login/Login";
 import Navbar from "./Components/Navbar";
 import Signup from "./pages/Signup/Signup";
