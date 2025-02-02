58f516f4321b5b16a28b3bae0051476dc2565398
Asad Ullah <shaikhasad183@gmail.com>
2025-02-02 12:26:10 +0500

    asad

---
+++ Backend/api/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();  // Load .env variables
+
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('../routes/auth.js');  // Path to your authRoutes
+const cors = require('cors');  // Import CORS
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());  // To parse JSON bodies
+app.use(cors());  // Allow all origins
+
+app.use('/api/auth', authRoutes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
